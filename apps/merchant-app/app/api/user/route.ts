ea31b6c242655f81c199dc053a14fac1d23d4412
Dev Dalia <devdalia9@gmail.com>
2025-03-08 15:12:32 +0530

    add: wprkflow

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -1,16 +1,14 @@
-import { NextResponse } from "next/server"
-import { PrismaClient } from "@repo/db/client";
-
-const client = new PrismaClient();
+import { NextResponse } from "next/server";
+import db from "@repo/db/client";
 
 export const GET = async () => {
-    await client.user.create({
-        data: {
-            email: "asd",
-            name: "adsads"
-        }
-    })
-    return NextResponse.json({
-        message: "hi there"
-    })
-}
+  // await db.user.create({
+  //   data: {
+  //     email: "asd",
+  //     name: "adsads",
+  //   },
+  // });
+  return NextResponse.json({
+    message: "hi there",
+  });
+};
--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -1,10 +1,7 @@
 "use client";
+// import { useBalance } from "@repo/store/balance";
 
-import { useBalance } from "@repo/store/balance";
-
-export default function() {
-  const balance = useBalance();
-  return <div>
-    hi there {balance}
-  </div>
+export default function () {
+  // const balance = useBalance();
+  return <div>hi there</div>;
 }
